b4da0e910d3fd7d147851fb1e8e39582f011fb83
nesssch <v.schattman@gmail.com>
2024-11-05 11:51:39 -0500

    fix: adds Firebase SDK

--- Starter_Application/app/build.gradle.kts
+++ Starter_Application/app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.google.gms.google.services)
+
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,4 +42,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
 }
